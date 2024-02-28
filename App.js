5e90709643c656d24f504a0a1e6cef45ec847900
ghk@seoulauction.com <ghk@seoulauction.com>
2024-02-28 15:05:50 +0900

    feat: SM2-1033 GNB 추가, UI Library React Native Elements 추가

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ButtonGroup } from '@rneui/themed';
+import React, {useState} from 'react'
 
 export default function App() {
+
+const [selectedIndex, setSelectedIndex] = useState(0);
+const [selectedIndexes, setSelectedIndexes] = useState([0, 2, 3, 4, 5, 6]);
   return (
     <View style={styles.container}>
       <Text>서울옥션 영업관리 App</Text>
       <StatusBar style="auto" />
+      <ButtonGroup
+      buttons={['로그인', '캘린더', '영업', '고객', '계약', '실적']}
+      selectedIndex={selectedIndex}
+      onPress={(value) => {
+        setSelectedIndex(value);
+      }}
+      containerStyle={{ marginBottom: 20 }}
+    />
     </View>
   );
 }
